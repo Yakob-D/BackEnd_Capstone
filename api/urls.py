5175cc49e02c7917fe40f04d1ee5a41d4763c788
Yakob <dibabuyakob@gmail.com>
2025-03-31 12:21:00 +0300

    Added rest_framework.authtoken into my INSTALLED_APPS

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,11 @@ from .views import CreateEventView, ListEventsView, UpdateEventView, DeleteEvent
 
 urlpatterns = [
     #path for GET request to list events
-    path('events/', ListEventsView.as_view(), name='event-list'),
+    path('/', ListEventsView.as_view(), name='event-list'),
     #path for POST request to create an event
-    path('events/create', CreateEventView.as_view(), name='event-create'),
+    path('create', CreateEventView.as_view(), name='event-create'),
     #path for PUT request to update an event with an id
-    path('events/update/<int:pk>', UpdateEventView.as_view(), name='event-update'),
+    path('update/<int:pk>', UpdateEventView.as_view(), name='event-update'),
     #path for DELETE request to delete an event with an id
-    path('events/delete/<int:pk>', DeleteEventView.as_view(), name='event-delete'),
+    path('delete/<int:pk>', DeleteEventView.as_view(), name='event-delete'),
 ]
