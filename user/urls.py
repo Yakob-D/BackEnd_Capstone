69bdbdd7eded124594bb41db977277ab9d1406c2
Yakob <dibabuyakob@gmail.com>
2025-03-31 15:01:31 +0300

    Defined a path for the LogoutView

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserView, LoginView
+from .views import RegisterUserView, LoginView, LogoutView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='user-login'),
+    path('logout/', LogoutView.as_view(), name='user-logout'),
 ]
